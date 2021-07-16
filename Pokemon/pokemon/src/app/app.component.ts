569b18fd5f4e270cfb9bc2a8eb2cf90b6e7cf1e8
khaled <khaledinat@gmail.com>
2021-07-16 16:31:41 +0200

    Ajout de Pokemon APP

---
+++ Pokemon/pokemon/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from './model/pokemon';
+import { POKEMONS } from './model/mock-pokemons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.pokemons = POKEMONS;
+  }
+
+  title = 'pokemon';
+  // @ts-ignore
+  pokemons: Pokemon[] = null;
+
+  selectPokemon(pokemon: Pokemon) {
+    console.log('Vous avez selectionné ' + pokemon.name);
+  }
+
+}
---
+++ Pokemon/pokemon/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import {PokemonTypeColorPipe  } from './pokemon-type-color.pipe';@NgModule({
+
+  declarations: [
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
